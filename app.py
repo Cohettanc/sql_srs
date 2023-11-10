9aac18a8e825af6722e4654dd8ed3e95d4a37929
Cohettanc <120392264+Cohettanc@users.noreply.github.com>
2023-11-10 19:03:34 +0100

    fix

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ option = st.selectbox("what would you like to review?",
                       index = None,
                       placeholder='Select a topic')
 
-tab1 = st.tabs(["SQL Quizz"])
 
-with tab1:
-    query_sql = st.text_area(label="entre ton input")
-    st.write(query_sql)
-    st.dataframe(duckdb.sql(query_sql).df())
+
+query_sql = st.text_area(label="entre ton input")
+st.write(query_sql)
+st.dataframe(duckdb.sql(query_sql).df())
